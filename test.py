b0f3a94da6be0da3b7ce1e6409462c86f5d85cca
Glyn Normington <glyn.normington@winchester.ac.uk>
2022-10-29 12:05:59 +0100

    Test Python version

---
+++ test.py
@@ -0,0 +1,12 @@
+import sys
+import unittest
+
+
+class TestPythonVersion(unittest.TestCase):
+
+    def test_python_version(self):
+        self.assertTrue(sys.version.startswith("3."), "Python version = %s" % sys.version)
+
+
+if __name__ == '__main__':
+    unittest.main()
